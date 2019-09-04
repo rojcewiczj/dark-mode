89a1da72eaf56a945957fa77c123d94edeb88dc0
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-09-04 14:57:15 -0500

    working on storage

--- src/hooks/LocalStorage.js
+++ src/hooks/LocalStorage.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 
 const LocalStorage = (key, initialValue) => {
- const [storedValue, setStoredValue] = useState(() => {
-     window
+ const [storedValue, setStoredValue] = useState(initialValue);
+ const setValue = value => {
+     window.localStorage.setItem(key, JSON.stringify(value))
+     setStoredValue(value);
  }
+ return [storedValue, setValue]
 };
 //useState(initialValue)------>[value, setValue]
 export default LocalStorage;
